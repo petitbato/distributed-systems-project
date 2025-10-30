562681dc2da08c9917155242763a44b9102e2357
Jan Schumacher Vincke <janschumacher98@gmail.com>
2025-10-30 17:29:00 +0100

    feat: implement home route and add index.html template with API documentation

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Kubernetes Pod! I want to be modified."
+def home():
+    return render_template('index.html')
+
+@app.route('/api/message', methods=['GET'])
+def get_message():
+    return jsonify({"message": "You get me"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
